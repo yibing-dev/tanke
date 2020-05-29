a1ac902ba72f0d1d041a1664ae6cc15cba6298e5
zhangyibing <zybgcs@126.com>
2020-05-29 13:02:11 +0800

    添加图标

---
+++ src/com/yibing/test/ImageTest.java
@@ -0,0 +1,25 @@
+package com.yibing.test;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+/**
+ * @createTime 2020年5月29日 下午12:47:24
+ * @author yibing
+ */
+public class ImageTest {
+	public static void main(String[] args) {
+		//BufferImage 从名字就可以看出来是读到缓存里的
+		try {
+			//把图片加载到内存里
+			BufferedImage bufferImage = ImageIO.read(new File("C:\\Users\\admin\\Desktop\\tank.jpg"));
+			
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
