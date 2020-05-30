ebbb3ac5532d735d842fd5def31663576a3a7741
1471405994@qq.com <1471405994@qq.com>
2020-05-30 11:12:58 +0800

    1.改造classLoader的获取方式 2.添加加载子弹icon的方法

--- src/com/yibing/tank/ResourceMgr.java
+++ src/com/yibing/tank/ResourceMgr.java
@@ -11,14 +11,27 @@ import javax.imageio.ImageIO;
  * @Description 类描述: 图片资源管理类
  */
 public class ResourceMgr {
+	// 坦克icon
 	static BufferedImage tankL, tankU, tankR, tankD;
+	//子弹icon
+	static BufferedImage bulletL, bulletU, bulletR, bulletD;
+	//定义一个ClassLoader，避免每一次都获取一次
+	static ClassLoader classLoader = null;
+	
 	// 定义静态代码块，在加载本类的时候回自动加载到内存里
 	static {
 		try {
-			tankL = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("images/tankL.gif"));
-			tankR = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("images/tankR.gif"));
-			tankU = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("images/tankU.gif"));
-			tankD = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("images/tankD.gif"));
+			classLoader = ResourceMgr.class.getClassLoader();
+			//加载坦克icon
+			tankL = ImageIO.read(classLoader.getResourceAsStream("images/tankL.gif"));
+			tankR = ImageIO.read(classLoader.getResourceAsStream("images/tankR.gif"));
+			tankU = ImageIO.read(classLoader.getResourceAsStream("images/tankU.gif"));
+			tankD = ImageIO.read(classLoader.getResourceAsStream("images/tankD.gif"));
+			//加载子弹icon
+			bulletL = ImageIO.read(classLoader.getResourceAsStream("images/bulletL.gif"));
+			bulletU = ImageIO.read(classLoader.getResourceAsStream("images/bulletU.gif"));
+			bulletR = ImageIO.read(classLoader.getResourceAsStream("images/bulletR.gif"));
+			bulletD = ImageIO.read(classLoader.getResourceAsStream("images/bulletD.gif"));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
