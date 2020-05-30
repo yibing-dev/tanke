6a0a7c3bdd9095db0060341f4d85a9f4fc5b7019
1471405994@qq.com <1471405994@qq.com>
2020-05-30 10:27:14 +0800

    加载图片测试类

--- src/com/yibing/test/ImageTest.java
+++ src/com/yibing/test/ImageTest.java
@@ -15,9 +15,11 @@ public class ImageTest {
 		//BufferImage 从名字就可以看出来是读到缓存里的
 		try {
 			//把图片加载到内存里
-			BufferedImage bufferImage = ImageIO.read(new File("C:\\Users\\admin\\Desktop\\tank.jpg"));
+			//BufferedImage bufferImage = ImageIO.read(new File("C:\\Users\\admin\\Desktop\\tank.jpg"));
 			
-			
+			//通过该classLoader把图片加载到内存里
+			BufferedImage image = ImageIO.read(ImageTest.class.getClassLoader().getResourceAsStream("images/bulletD.gif"));
+			System.out.println(image.getHeight());
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
