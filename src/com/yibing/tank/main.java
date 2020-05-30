018eb659666192a4f42df7b2062d526284857948
1471405994@qq.com <1471405994@qq.com>
2020-05-30 10:28:00 +0800

    更新

--- src/com/yibing/tank/main.java
+++ src/com/yibing/tank/main.java
@@ -7,7 +7,6 @@ package com.yibing.tank;
 public class main {
 	public static void main(String[] args) throws InterruptedException {
 		TankFrame f = new TankFrame();
-
 		while (true) {
 			Thread.sleep(50);
 			f.repaint();
