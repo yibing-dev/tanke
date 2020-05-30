83110449d1b0a770aae1405eddc2cd718bdfad2f
1471405994@qq.com <1471405994@qq.com>
2020-05-30 11:11:51 +0800

    1.将子弹换成图片 2.根据不同的按键方向，显示不同方向的子弹icon

--- src/com/yibing/tank/Bullet.java
+++ src/com/yibing/tank/Bullet.java
@@ -13,12 +13,12 @@ public class Bullet {
 	private Dir dir;
 	private static final int WIDTH = 30, HEIGHT = 30;
 	// 子弹活着
-	private boolean live = true;
+	private boolean live = true; 
 	TankFrame tf = null;
 
 	public Bullet(int x, int y, Dir dir, TankFrame tf) {
 		this.x = x;
-		this.y = y;
+		this.y = y; 
 		this.dir = dir;
 		this.tf = tf;
 	}
@@ -28,10 +28,23 @@ public class Bullet {
 			tf.bullets.remove(this);
 		}
 
-		Color c = g.getColor();
-		g.setColor(Color.RED);// 设置颜色
-		g.fillOval(x, y, WIDTH, HEIGHT);// 设置出现的初始位置
-		g.setColor(c);// 还原画笔的颜色
+		switch (dir) {
+		case LEFT:
+			g.drawImage(ResourceMgr.bulletL, x, y, null);
+			break;
+		case RIGHT:
+			g.drawImage(ResourceMgr.bulletR, x, y, null);
+			break;
+		case UP:
+			g.drawImage(ResourceMgr.bulletU, x, y, null);
+			break;
+		case DOWN:
+			g.drawImage(ResourceMgr.bulletD, x, y, null);
+			break;
+		default:
+			break;
+
+		}
 		move();
 	}
 
